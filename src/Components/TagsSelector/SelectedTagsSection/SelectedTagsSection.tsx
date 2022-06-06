dffbd02747a7ae70de89a5ff4afaac81b39a9348
michalbie <userofficeit@gmail.com>
2022-06-06 22:16:56 +0200

    Remove unnecesary files

--- src/Components/TagsSelector/SelectedTagsSection/SelectedTagsSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./SelectedTagsSection.css";
-import TagMarker from "../TagMarker/TagMarker";
-
-function SelectedTagsSection(): JSX.Element {
-	return <section className="selected-tags-section"></section>;
-}
-
-export default SelectedTagsSection;
